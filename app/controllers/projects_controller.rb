54672a2ead274cbda696963a206252574eb24db5
Adam Kurniawan <dmkrnwn@gmail.com>
2016-03-21 23:37:57 +0700

    Added paperclip as image uplod and add migration to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -41,7 +41,7 @@ class ProjectsController < ApplicationController
 
 	private
 		def project_params
-			params.require(:project).permit(:title, :description)
+			params.require(:project).permit(:title, :description, :image)
 		end
 
 		def find_project
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317073633) do
+ActiveRecord::Schema.define(version: 20160321161204) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,9 +24,13 @@ ActiveRecord::Schema.define(version: 20160317073633) do
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
